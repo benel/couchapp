92edbdf9eae9b530fd5d3dc832c179dce4f87014
Aurelien Benel <aurelien.benel@utt.fr>
2021-02-02 14:47:47 +0100

    FIX: couchapp should not be started while the database is being created.

--- patient_couchapp.sh
+++ patient_couchapp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -n "$URI" ]; then
-  until $(curl --output /dev/null --fail --silent $URI || curl --request PUT --output /dev/null --silent $URI); do
+  until $(curl --output /dev/null --silent $URI); do
       printf '.'
       sleep 2
   done
